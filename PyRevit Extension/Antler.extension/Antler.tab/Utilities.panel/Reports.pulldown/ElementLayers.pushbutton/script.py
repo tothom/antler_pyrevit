812cb87898db2ce32af803c45b39e0407cd76bea
Thomas Holth <thomas.holth@gmail.com>
2021-07-07 16:08:08 +0200

    Bug fixes. Element expand/query command.

--- PyRevit Extension/Antler.extension/Antler.tab/Utilities.panel/Reports.pulldown/ElementLayers.pushbutton/script.py
+++ PyRevit Extension/Antler.extension/Antler.tab/Utilities.panel/Reports.pulldown/ElementLayers.pushbutton/script.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import antler
 from rpw import revit, DB, UI
-from pyrevit import forms, script
+from pyrevit import forms, script, EXEC_PARAMS
 import re
+
+uidoc = revit.uidoc
+doc = revit.doc
 
 logger = script.get_logger()
 
@@ -11,62 +15,81 @@ logger = script.get_logger()
 # if not lib_path in sys.path:
 #     sys.path.append(lib_path)
 
-import antler
 
 __doc__ = "Get Element Material Layers"
 __title__ = "Element Layers"
 __author__ = "Thomas Holth"
 
-ELEMENT_TYPE_CONVERSION_DICT = {
-    DB.Wall: 'WallType',
-    DB.Floor: 'FloorType',
-    # DB.Roof: 'RoofType',
-    DB.FootPrintRoof: 'RoofType',
-    DB.Ceiling: 'Ceiling',
-}
+# ELEMENT_TYPE_CONVERSION_DICT = {
+#     DB.Wall: 'WallType',
+#     DB.Floor: 'FloorType',
+#     # DB.Roof: 'RoofType',
+#     DB.FootPrintRoof: 'RoofType',
+#     DB.Ceiling: 'Ceiling',
+# }
 
-elements = [revit.doc.GetElement(id) for id in revit.uidoc.Selection.GetElementIds()]
 
-# element = elements[0]  # TODO: Modify script for multiple elements.
+def report_layer_structure(element):
+    """
+    """
+    logger.info(type(element))
+
+    element_type_id = element.GetTypeId()
+
+    element_type = doc.GetElement(element_type_id)
+
+    if not element_type:
+        return None
+
+    # element_type = eval(
+    #     'element.' + ELEMENT_TYPE_CONVERSION_DICT[type(element)])
+    # logger.info(element_type)
+
+    # Collect layer information
+    compound_structure = element_type.GetCompoundStructure()
+
+    layers_dict = antler.interop.compound_structure_to_dict(
+        compound_structure)
+
+    # Print layer information
+    antler.ui.print_dict_list([a.get('Material')
+                              for a in layers_dict.values()])
+
+    # Write layer string to parameter
+    build_list = []
+
+    for layer in layers_dict.values():
+        layer_string = '-'
+
+        material = layer['Material']
+
+        if material:
+            description = material['Description']
+
+            if description:
+                layer_string = description
+
+        logger.info(layer_string)
+        build_list.append(layer_string)
+
+    return build_list
+
+
+selection = uidoc.Selection.GetElementIds() or uidoc.Selection.PickObjects(
+    UI.Selection.ObjectType.Element, "Select objects to check.")
+
+elements = [doc.GetElement(id) for id in selection]
 
 with DB.Transaction(revit.doc, __title__) as t:
     t.Start()
 
     for element in elements:
+        build_list = report_layer_structure(element)
 
-        logger.info(type(element))
+        if EXEC_PARAMS.config_mode:
+            build_string = '\r\n'.join(build_list)
 
-        element_type = eval('element.' + ELEMENT_TYPE_CONVERSION_DICT[type(element)])
-        logger.info(element_type)
-
-        # Collect layer information
-        compound_structure = element_type.GetCompoundStructure()
-
-        layers_dict = antler.interop.compound_structure_to_dict(compound_structure)
-
-        # Print layer information
-        antler.ui.print_dict_list([a.get('M,aterial') for a in layers_dict.values()])
-
-        # Write layer string to parameter
-        build_list = []
-
-        for layer in layers_dict.values():
-            layer_string = '-'
-
-            material = layer['Material']
-
-            if material:
-                description = material['Description']
-
-                if description:
-                    layer_string = description
-
-            logger.info(layer_string)
-            build_list.append(layer_string)
-
-        build_string = '\r\n'.join(build_list)
-
-        parameter = element_type.LookupParameter('Oppbygning')
-        parameter.Set(build_string)
+            parameter = element_type.LookupParameter('Oppbygning')
+            parameter.Set(build_string)
 
     t.Commit()
---
+++ PyRevit Extension/Antler.extension/Antler.tab/Utilities.panel/Reports.pulldown/ExpandElement.pushbutton/script.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+
+from rpw import revit, DB, UI
+from pyrevit import forms, script, EXEC_PARAMS
+
+import antler
+
+uidoc = revit.uidoc
+doc = revit.doc
+
+logger = script.get_logger()
+
+
+def get_properties(obj):
+    pass
+
+
+selection = uidoc.Selection.GetElementIds() or uidoc.Selection.PickObjects(
+    UI.Selection.ObjectType.Element, "Select objects to expand.")
+
+elements = [doc.GetElement(id) for id in selection]
+
+for element in elements:
+    print("Element: {}".format(element))
+
+    print("\n\t Instance Parameters")
+    parameter_dict = {parameter.Definition.Name: parameter.AsString(
+    ) or parameter.AsValueString() for parameter in element.Parameters}
+
+    for k, v in parameter_dict.items():
+        print(k, v)
+
+    # print("\n\t dir(element.Parameters)")
+    # for attr in dir(element.Parameters):
+    #     try:
+    #         value = getattr(element.Parameters, attr)
+    #         if not callable(value):
+    #             print(attr, value, type(value))  # , callable(value))
+    #     except Exception as e:
+    #         print(e)
+
+    print("\n\t dir() -> Element Properties")
+    for attr in dir(element):
+        try:
+            value = getattr(element, attr)
+            if not callable(value):
+                print(attr, value, type(value))  # , callable(value))
+        except Exception as e:
+            print(e)
+
+    try:
+        element_type = doc.GetElement(element.GetTypeId())
+        print("Element Type: {}".format(element_type))
+
+        print("\n\t Type Parameters")
+        type_parameter_dict = {parameter.Definition.Name: parameter.AsString(
+        ) or parameter.AsValueString() for parameter in element_type.Parameters}
+
+        # print("\n".join(["{}: {}".format(k, v)
+        #       for k, v in type_parameter_dict.items()]))
+        for k, v in type_parameter_dict.items():
+            print(k, v)
+
+        print("\n\t dir() -> Type Properties")
+        for attr in dir(element):
+            try:
+                value = getattr(element, attr)
+                if not callable(value):
+                    print(attr, value, type(value))  # , callable(value))
+            except Exception as e:
+                print(e)
+
+    except:
+        print("Element does not have a Type...\n")
