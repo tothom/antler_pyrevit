1d1240119c4eb7ad69f3cfa05461a1dcab3beb52
Thomas Holth <thomas.holth@gmail.com>
2021-08-10 16:40:42 +0200

    Transform by coordinates command.

---
+++ PyRevit Extension/Antler.extension/Antler.tab/Modify.panel/straighten.stack/TransformByCoordinates.pushbutton/script.py
@@ -0,0 +1,37 @@
+from System.Collections.Generic import List
+from rpw import revit, DB, UI
+
+from pyrevit import forms, script, EXEC_PARAMS
+
+import math
+# import clr
+import antler.transform
+
+uidoc = revit.uidoc
+doc = revit.doc
+
+logger = script.get_logger()
+config = script.get_config()
+
+units = doc.GetUnits()
+print(units)
+
+# Select Elements to straighten
+selection = uidoc.Selection.PickObject(UI.Selection.ObjectType.Element)
+element = doc.GetElement(selection)
+
+coordinates = forms.ask_for_string()
+coordinates = [float(a)/304.8 for a in coordinates.split(',')]
+translation_vector = DB.XYZ(coordinates[0], coordinates[1], 0)
+
+
+
+# transform = DB.Transform.CreateTranslation()
+
+# if guides:
+with DB.Transaction(doc, __commandname__) as t:
+    t.Start()
+
+    result = DB.ElementTransformUtils.MoveElement(doc, element.Id, translation_vector)
+
+    t.Commit()
