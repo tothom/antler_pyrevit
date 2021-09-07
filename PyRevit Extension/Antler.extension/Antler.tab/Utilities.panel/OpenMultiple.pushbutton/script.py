d5d4c695221f0627465299e8f1134e4a664845ba
Thomas Holth <thomas.holth@gmail.com>
2021-09-07 15:56:55 +0200

    Open multiple file types

--- PyRevit Extension/Antler.extension/Antler.tab/Utilities.panel/OpenMultiple.pushbutton/script.py
+++ PyRevit Extension/Antler.extension/Antler.tab/Utilities.panel/OpenMultiple.pushbutton/script.py
@@ -9,20 +9,22 @@ from Microsoft.Win32 import OpenFileDialog
 logger = script.get_logger()
 output = script.get_output()
 
+
 def rvt_file_dialog():
     dialog = OpenFileDialog()
-    dialog.Filter = "Revit model (.rvt)|*.rvt" # Filter files by extension
+    # Filter files by extension
+    dialog.Filter = "Revit file types (*.rvt, *.rfa, *.rte)|*.rvt;*.rfa;*.rte"
     dialog.Multiselect = True
 
     result = dialog.ShowDialog()
 
     return dialog.FileNames
 
+
 filenames = rvt_file_dialog()
 
-if filenames:
-    for filename in filenames:
-        try:
-            revit.uiapp.OpenAndActivateDocument(filename)
-        except Exception as e:
-            logger.warning(e)
+for filename in filenames:
+    try:
+        revit.uiapp.OpenAndActivateDocument(filename)
+    except Exception as e:
+        logger.warning(e)
