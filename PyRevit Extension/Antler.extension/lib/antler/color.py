52d0fce908b233ba3fc139db76c66ca9b152c628
Thomas Holth <thomas.holth@gmail.com>
2021-10-15 15:15:35 +0200

    Deleted HSL function

--- PyRevit Extension/Antler.extension/lib/antler/color.py
+++ PyRevit Extension/Antler.extension/lib/antler/color.py
@@ -63,44 +63,7 @@ def rgb_to_hsv(r, g, b):
     return h, s, v
 
 
-def hsl_to_rgb(h, s, l):
-    """
-    Input:
-        h: Hue as angle, 0 < h < 2*pi
-        s: Saturation as a float, 0 < s <1.0
-        l: Lightness as a float, 0 < v <1.0
 
-    Returns: RGB as tuble.
-    """
-    def _v(m1, m2, hue):
-        hue = hue % 1.0
-
-        if hue < 1.0/6.0:
-            return m1 + (m2 - m1) * hue * 6.0
-        elif hue < 0.5:
-            return m2
-        elif hue < 2.0/3.0:
-            return m1 + (m2 - m1) * (2.0/3.0 - hue) * 6.0
-        else:
-            return m1
-
-    h = h / (2*math.pi)
-
-    if s == 0.0:
-        return 1.0, 1.0, 1.0  # Isn't this wrong?
-
-    if l <= 0.5:
-        m2 = l * (1.0 + s)
-    else:
-        m2 = l + s - (l * s)
-
-    m1 = 2.0*l - m2
-
-    return (
-        _v(m1, m2, h + 1.0/3.0),
-        _v(m1, m2, h),
-        _v(m1, m2, h - 1.0/3.0)
-        )
 
 
 def lighten_color(color, factor=0.5):
