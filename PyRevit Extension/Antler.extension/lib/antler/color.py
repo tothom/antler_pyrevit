dce16189d47250eafa098ef9c7a54b2e5534f0a4
Thomas Holth <thomas.holth@gmail.com>
2021-10-15 16:09:24 +0200

    Color library improvements

--- PyRevit Extension/Antler.extension/lib/antler/color.py
+++ PyRevit Extension/Antler.extension/lib/antler/color.py
@@ -1,9 +1,9 @@
+import random
 import math
+import clr
+clr.AddReference("System.Drawing")
 
-# import clr
-#
-# clr.AddReference("System.Drawing")
-# import System.Drawing
+from System.Drawing import Color  # noqa: E402
 
 
 def hsv_to_rgb(h, s, v):
@@ -18,9 +18,12 @@ def hsv_to_rgb(h, s, v):
         stackoverflow.com/questions/1335426/is-there-a-built-in-c-net-system-api-for-hsv-to-rgb
 
     Usage:
-    hsv_to_rgb(3.14, 0.5, 1)
+    hsv_to_rgb(1.0, 0.5, 0.3)
     """
     # h = h % (2*math.pi)
+    h = max(h, 0) and min(h, 1)
+    s = max(s, 0) and min(s, 1)
+    v = max(v, 0) and min(v, 1)
 
     hi = math.floor(h * 6.0) % 6
     f = h * 6.0 - math.floor(h * 6.0)
@@ -47,29 +50,40 @@ def rgb_to_hsv(r, g, b):
     maxc = max(r, g, b)
     minc = min(r, g, b)
     v = maxc
+
     if minc == maxc:
         return 0.0, 0.0, v
-    s = (maxc-minc) / maxc
-    rc = (maxc-r) / (maxc-minc)
-    gc = (maxc-g) / (maxc-minc)
-    bc = (maxc-b) / (maxc-minc)
+
+    s = (maxc - minc) / maxc
+    rc = (maxc - r) / (maxc - minc)
+    gc = (maxc - g) / (maxc - minc)
+    bc = (maxc - b) / (maxc - minc)
+
     if r == maxc:
-        h = bc-gc
+        h = bc - gc
     elif g == maxc:
-        h = 2.0+rc-bc
+        h = 2.0 + rc - bc
     else:
-        h = 4.0+gc-rc
-    h = (h/6.0) % 1.0
+        h = 4.0 + gc - rc
+
+    h = (h / 6.0) % 1.0
+
     return h, s, v
 
 
+def relative_color_hsv(color, dh=0.0, ds=0.0, dv=0.0):
+    h, s, v = rgb_to_hsv(color.R/255, color.G/255, color.B/255)
+
+    h += dh
+    s += ds
+    v += dv
+
+    r, g, b = hsv_to_rgb(h, s, v)
+
+    return Color.FromArgb(int(r*255), int(g*255), int(b*255))
 
 
-
-def lighten_color(color, factor=0.5):
-    r, g, b = color.R, color.G, color.B
-    # return System.Drawing.Color.FromArgb(r, g, b)
-
-
-def random_color(seed):
+def random_color(seed, h=None, s=None, v=None):
+    if h is None:
+        h = random.random()
     pass
