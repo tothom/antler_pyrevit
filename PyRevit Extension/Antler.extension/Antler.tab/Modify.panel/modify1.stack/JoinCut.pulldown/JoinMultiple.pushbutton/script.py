3a59285be47f2147bfe6769002b79ae8d7aad460
thomas.holth <thomas.holth@gmail.com>
2022-01-14 17:44:15 +0100

    Better feedback

--- PyRevit Extension/Antler.extension/Antler.tab/Modify.panel/modify1.stack/JoinCut.pulldown/JoinMultiple.pushbutton/script.py
+++ PyRevit Extension/Antler.extension/Antler.tab/Modify.panel/modify1.stack/JoinCut.pulldown/JoinMultiple.pushbutton/script.py
@@ -13,6 +13,7 @@ uidoc = revit.uidoc
 doc = revit.doc
 
 logger = script.get_logger()
+output = script.get_output()
 
 if uidoc.Selection.GetElementIds():
     elements = [doc.GetElement(id) for id in uidoc.Selection.GetElementIds()]
@@ -24,13 +25,31 @@ else:
 
 
 if elements:
+    # output.indeterminate_progress(True)
+    element_combinations = combinations(elements, 2)
+    length = sum(1 for _ in combinations(elements, 2))
+    i = 1
+
     with DB.Transaction(doc, __commandname__) as t:
         t.Start()
+
         for combination in combinations(elements, 2):
             try:
                 DB.JoinGeometryUtils.JoinGeometry(
                     doc, combination[0], combination[1])
             except Exception as e:
-                logger.warning("{}: {}".format(type(e), e))
+                logger.debug("{}: {}".format(type(e), e))
+            # else:
+            #     logger.info("{a} and {b} succesfully joined.".format(
+            #         a=combination[0],
+            #         b=combination[1],
+            #         # a=output.linkify(combination[0].Id),
+            #         # b=output.linkify(combination[1].Id),
+            #         )
+            #     )
+
+            # output.indeterminate_progress(False)
+            output.update_progress(i, length)
+            i += 1
 
         t.Commit()
