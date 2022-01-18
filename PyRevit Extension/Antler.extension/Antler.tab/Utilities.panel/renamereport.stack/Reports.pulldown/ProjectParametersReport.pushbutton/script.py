a0e520fedb582e88f255d7de76617111d9340e5c
thomas.holth <thomas.holth@gmail.com>
2022-01-18 13:56:04 +0100

    Project parameters report

---
+++ PyRevit Extension/Antler.extension/Antler.tab/Utilities.panel/renamereport.stack/Reports.pulldown/ProjectParametersReport.pushbutton/script.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Source: https://thebuildingcoder.typepad.com/blog/2015/12/shared-project-parameter-guid-reporter.html
+"""
+
+from rpw import revit, DB, UI
+from pyrevit import forms, script
+# import re
+
+import antler.util
+
+logger = script.get_logger()
+
+report = []
+
+iterator = revit.doc.ParameterBindings.ForwardIterator()
+iterator.Reset()
+
+while iterator.MoveNext():
+    definition = iterator.Key
+    binding = iterator.Current
+    parameter = revit.doc.GetElement(definition.Id)
+
+    record = {}
+
+    record['Binding Type'] = 'Instance' if isinstance(binding, DB.InstanceBinding) else 'Type'
+    try:
+        categories = [category.Name for category in binding.Categories]
+    except Exception as e:
+        logger.debug(e)
+
+    record['Categories'] = categories
+
+    record['Name'] = definition.Name
+    record['Parameter Type'] = definition.ParameterType
+
+    record['Is Shared Parameter'] = isinstance(parameter, DB.SharedParameterElement)
+
+    if record['Is Shared Parameter']:
+        record['GUID'] = parameter.GuidValue
+
+    # logger.info(record)
+
+    report.append(record)
+
+antler.util.print_dict_list(report)
