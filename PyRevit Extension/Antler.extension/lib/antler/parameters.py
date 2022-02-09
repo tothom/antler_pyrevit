4cdc5ad94b1e764ff5b8b97ea1aeac88c6ab2334
tothom <mail@thomasholth.no>
2022-02-09 19:31:50 +0100

    Added optional convert to/from internal units

--- PyRevit Extension/Antler.extension/lib/antler/parameters.py
+++ PyRevit Extension/Antler.extension/lib/antler/parameters.py
@@ -5,7 +5,7 @@ from pyrevit import forms, script
 logger = script.get_logger()
 
 
-def get_parameter_value(parameter):
+def get_parameter_value(parameter, convert=True):
     PARAMETER_GET_MAPPING = {
         DB.StorageType.Integer: DB.Parameter.AsInteger,
         DB.StorageType.Double: DB.Parameter.AsDouble,
@@ -22,17 +22,21 @@ def get_parameter_value(parameter):
 
     logger.debug("Internal value: {}".format(internal_value))
 
-    try:
-        value = DB.UnitUtils.ConvertFromInternalUnits(
-            internal_value, parameter.DisplayUnitType)
-    except RevitExceptions.InvalidOperationException as e:
-        # logger.warning((type(e), e))
-        value = internal_value
+    if convert:
+        try:
+            value = DB.UnitUtils.ConvertFromInternalUnits(
+                internal_value, parameter.DisplayUnitType)
+        except RevitExceptions.InvalidOperationException as e:
+            # logger.warning((type(e), e))
+            value = internal_value
 
-    return value
+        return value
+        
+    else:
+        return internal_value
 
 
-def set_parameter_value(parameter, value):
+def set_parameter_value(parameter, value, convert=True):
     PARAMETER_SET_MAPPING = {
         DB.StorageType.Integer: int,
         DB.StorageType.Double: float,
@@ -42,17 +46,53 @@ def set_parameter_value(parameter, value):
 
     value = PARAMETER_SET_MAPPING[parameter.StorageType](value)
 
-    try:
-        internal_value = DB.UnitUtils.ConvertToInternalUnits(
-            value, parameter.DisplayUnitType)
-    except RevitExceptions.InvalidOperationException as e:
-        # logger.warning((type(e), e))
-        internal_value = value
+    if convert:
+        try:
+            internal_value = DB.UnitUtils.ConvertToInternalUnits(
+                value, parameter.DisplayUnitType)
+        except RevitExceptions.InvalidOperationException as e:
+            # logger.warning((type(e), e))
+            internal_value = value
+        parameter.Set(internal_value)
 
+    else:
+        parameter.Set(value)
     # if not parameter.IsReadOnly:
-    parameter.Set(internal_value)
 
 
-def get_common_parameters(elements):
-    common_parameters = []
-    pass
+
+def get_common_parameter_definitions(elements, filter_function=lambda x:True):
+    """
+    Returns common parameter definitions for input elements. Example if all
+    elements has a parameter for Area, the Area parameter definition will be
+    returned.
+
+    The filter function takes the parameter as argument.
+    """
+    valid_definition_sets = []
+    valid_definitions_dict = {}
+
+    for element in elements:
+        valid_definition_set = set()
+
+        for parameter in element.Parameters:
+
+            logger.debug("Checking {definition}: {storage_type}".format(
+                definition=parameter.Definition.Name,
+                storage_type=parameter.StorageType
+            ))
+
+            if filter_function(parameter):
+                valid_definition_set.add(parameter.Definition.Id)
+                valid_definitions_dict[parameter.Definition.Id] = parameter.Definition
+
+                logger.debug("Definition {definition} add to set.".format(
+                    definition=parameter.Definition.Name))
+
+        valid_definition_sets.append(valid_definition_set)
+
+    logger.debug(valid_definition_sets)
+
+    common_parameter_definitions = [valid_definitions_dict[id] for id in set.intersection(*valid_definition_sets)]
+
+    return list(common_parameter_definitions)
