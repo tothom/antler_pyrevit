4731c2898e3a1a5a67638a96e485040f6352fa89
tothom <mail@thomasholth.no>
2022-02-09 19:32:43 +0100

    Multiple cutters possible

--- PyRevit Extension/Antler.extension/Antler.tab/Modify.panel/modify1.stack/JoinCut.pulldown/CutMultiple.pushbutton/script.py
+++ PyRevit Extension/Antler.extension/Antler.tab/Modify.panel/modify1.stack/JoinCut.pulldown/CutMultiple.pushbutton/script.py
@@ -1,7 +1,7 @@
 from System.Collections.Generic import *
 from rpw import revit, DB, UI
 
-from pyrevit import forms
+from pyrevit import forms, script
 
 from collections import OrderedDict
 from System.Collections.Generic import List
@@ -13,28 +13,40 @@ __author__ = "Thomas Holth"
 uidoc = revit.uidoc
 doc = revit.doc
 
+logger = script.get_logger()
+output = script.get_output()
+
 
 if uidoc.Selection.GetElementIds():
     elements = [doc.GetElement(id) for id in uidoc.Selection.GetElementIds()]
 else:
-    references = uidoc.Selection.PickObjects(
-        UI.Selection.ObjectType.Element, "Select elements to be cut...")
-    elements = [doc.GetElement(reference.ElementId)
+    try:
+        references = uidoc.Selection.PickObjects(
+            UI.Selection.ObjectType.Element, "Select elements TO BE CUT...")
+    except:
+        script.exit()
+    else:
+        elements = [doc.GetElement(reference.ElementId)
                 for reference in references]
 
-cutter_reference = uidoc.Selection.PickObject(
-    UI.Selection.ObjectType.Element, "Select cutter...")
-cutter = doc.GetElement(cutter_reference.ElementId)
+cutter_references = uidoc.Selection.PickObjects(
+    UI.Selection.ObjectType.Element, "Select CUTTERS...")
+
+cutters = [doc.GetElement(a.ElementId) for a in cutter_references]
 
 
 if elements:
     with DB.Transaction(doc, __title__) as t:
         t.Start()
         for element in elements:
-            try:
-                DB.InstanceVoidCutUtils.AddInstanceVoidCut(
-                    doc, element, cutter)
-            except Exception as e:
-                #print(e)
-                pass
+            for cutter in cutters:
+                try:
+                    DB.InstanceVoidCutUtils.AddInstanceVoidCut(
+                        doc, element, cutter)
+                except Exception as e:
+                    logger.warning("Cutting of {element} with {cutter} failed.".format(
+                        element=output.linkify(element.Id),
+                        cutter=output.linkify(cutter.Id),
+                    ))
+                    logger.warning(e)
         t.Commit()
