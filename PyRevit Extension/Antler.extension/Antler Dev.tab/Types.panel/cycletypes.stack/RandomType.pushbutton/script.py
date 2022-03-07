72de01541660522099a04822c4f54b0044943d23
tothom <mail@thomasholth.no>
2022-03-07 11:53:28 +0100

    Random type command fix

--- PyRevit Extension/Antler.extension/Antler Dev.tab/Types.panel/cycletypes.stack/RandomType.pushbutton/script.py
+++ PyRevit Extension/Antler.extension/Antler Dev.tab/Types.panel/cycletypes.stack/RandomType.pushbutton/script.py
@@ -6,6 +6,8 @@ from rpw import revit, DB, UI
 from pyrevit import forms
 from collections import OrderedDict
 
+import random
+
 import time
 start_time = time.time()
 
@@ -19,33 +21,15 @@ uidoc = revit.uidoc
 doc = revit.doc
 
 
-def cycle_family_type(element, next=True):
+def random_type(element, next=True):
     family = element.Symbol.Family
 
-    symbol_ids = family.GetFamilySymbolIds()
-    symbols = [doc.GetElement(id) for id in symbol_ids]
-    symbol_names = [s.get_Parameter(
-        DB.BuiltInParameter.SYMBOL_NAME_PARAM).AsString() for s in symbols]
+    symbol_ids = list(family.GetFamilySymbolIds())
 
-    symbol_id_dict = {name: id for name, id in zip(symbol_names, symbol_ids)}
-    symbol_id_dict = OrderedDict(sorted(symbol_id_dict.items()))
-
-    symbol_dict = {name: symbol for name, symbol in zip(symbol_names, symbols)}
-    symbol_dict = OrderedDict(sorted(symbol_dict.items()))
+    symbol_id = random.choice(symbol_ids)
 
 
-    index = symbol_id_dict.values().index(element.Symbol.Id)
-
-    if next:
-        next_index = index + 1
-    else:
-        next_index = index - 1
-
-    next_index = next_index % len(symbol_ids)
-
-    next_symbol = symbol_dict.values()[next_index]
-
-    element.Symbol = next_symbol
+    element.Symbol = doc.GetElement(symbol_id)
 
     return element
 
@@ -59,7 +43,7 @@ if elements:
             t.Start()
 
             for element in elements:
-                cycle_family_type(element, next=True)
+                random_type(element, next=True)
 
             t.Commit()
 
