b73e649dcd277819fdcd0e1930910ca9279724af
thomas.holth <thomas.holth@gmail.com>
2022-03-17 18:20:23 +0100

    Set sheet titleblock command

--- PyRevit Extension/Antler.extension/lib/antler/util.py
+++ PyRevit Extension/Antler.extension/lib/antler/util.py
@@ -60,6 +60,11 @@ def builtin_category_from_category(category):
     """
     Returns corresponding Builtin Category from Category.
     """
+    logger.debug(type(category))
+
+    if isinstance(category, DB.BuiltInCategory):
+        return category
+
     for builtin_category in System.Enum.GetValues(DB.BuiltInCategory):
         if DB.ElementId(builtin_category).IntegerValue == category.Id.IntegerValue:
             return builtin_category
