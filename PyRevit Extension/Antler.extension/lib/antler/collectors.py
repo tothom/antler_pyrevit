307201c1c6359cdbc1762beb03e87f42d410cfe5
thomas.holth <thomas.holth@gmail.com>
2022-04-28 10:58:05 +0200

    More collectors and element getters

--- PyRevit Extension/Antler.extension/lib/antler/collectors.py
+++ PyRevit Extension/Antler.extension/lib/antler/collectors.py
@@ -1,16 +1,16 @@
+import filters
+import util
+from collections import OrderedDict
+
 from rpw import revit, DB
 from pyrevit import script
 
 import clr
 
-clr.AddReference("System.Core")
-import System
+# clr.AddReference("System.Core")
+import System.Linq
 clr.ImportExtensions(System.Linq)
 
-from collections import OrderedDict
-
-import util
-import filters
 
 logger = script.get_logger()
 
@@ -36,6 +36,9 @@ def elements_of_class_collector(revit_class, select_types=True, doc=revit.doc, *
     return collector
 
 
+# def
+
+
 def elements_on_level_collector(level, doc=revit.doc):
     """
     """
@@ -48,7 +51,7 @@ def elements_on_level_collector(level, doc=revit.doc):
 
 def revit_link_instances_collector(doc=revit.doc):
     collector = DB.FilteredElementCollector(doc)
-    #collector.OfCategory(DB.BuiltInCategory.OST_RvtLinks)
+    # collector.OfCategory(DB.BuiltInCategory.OST_RvtLinks)
     collector.OfClass(clr.GetClrType(DB.RevitLinkInstance))
 
     return collector
@@ -56,7 +59,7 @@ def revit_link_instances_collector(doc=revit.doc):
 
 def revit_link_types_collector(doc=revit.doc):
     collector = DB.FilteredElementCollector(doc)
-    #collector.OfCategory(DB.BuiltInCategory.OST_RvtLinks)
+    # collector.OfCategory(DB.BuiltInCategory.OST_RvtLinks)
     collector.OfClass(clr.GetClrType(DB.RevitLinkType))
 
     return collector
@@ -76,27 +79,15 @@ def collect_instances_of_element_type(element_type):
     """
     collector = DB.FilteredElementCollector(element_type.Document)
     collector.WhereElementIsNotElementType()
-    collector.OfCategory(util.builtin_category_from_category(element_type.Category))
+    collector.OfCategoryId(element_type.Category.Id)
 
-    enumerable = collector.Where(lambda e: e.GetTypeId().IntegerValue.Equals(element_type.Id.IntegerValue))
+    # collector.Where(lambda e: e.GetTypeId().IntegerValue == element_type.Id.IntegerValue)
+
+    enumerable = collector.ToElements().Where(
+        lambda e: e.GetTypeId().IntegerValue == element_type.Id.IntegerValue)
 
     return enumerable.ToList()
 
-
-def get_view_by_name(name, doc=revit.doc):
-    collector = DB.FilteredElementCollector(doc)
-    collector.OfClass(DB.View)
-    collector.WhereElementIsNotElementType()
-
-    collector.WherePasses(filter.view_name_filter(name))
-
-    count = collector.GetElementCount()
-
-    if count==1:
-        return collector.FirstElement()
-    else:
-        logger.warning("More than one view found.")
-        raise KeyError
 
 def view_template_collector(doc=revit.doc):
     collector = DB.FilteredElementCollector(doc)
@@ -107,7 +98,6 @@ def view_template_collector(doc=revit.doc):
     collector.Where(lambda x: x.IsTemplate)
 
     return collector
-
 
 
 def collect_view_templates(doc=revit.doc):
@@ -137,3 +127,115 @@ def room_collector(doc=revit.doc, phase_id=None):
         collector.WherePasses(filter.room_phase_filter(phase_id))
 
     return collector
+
+
+def collect_project_parameters(doc=revit.doc):
+    parameter_bindings_dict = {}
+
+    iterator = doc.ParameterBindings.ForwardIterator()
+    iterator.Reset()
+
+    while iterator.MoveNext():
+        parameter_bindings_dict[iterator.Key] = iterator.Current
+
+    return parameter_bindings_dict
+
+
+def area_schemes_collector(doc=revit.doc):
+    collector = DB.FilteredElementCollector(doc)
+    collector.OfClass(DB.AreaScheme)
+
+    return collector
+
+
+def areas_of_area_scheme_collector(area_scheme):
+    doc = area_scheme.Document
+
+    collector = DB.FilteredElementCollector(doc)
+    collector.OfClass(DB.Area).WhereElementIsNotElementType()
+
+    provider = DB.ParameterValueProvider(
+        DB.ElementId(DB.BuiltInParameter.AREA_SCHEME_ID))
+    rule = DB.FilterElementIdRule(
+        provider, DB.FilterNumericEquals(), area_scheme.Id)
+    parameter_filter = DB.ElementParameterFilter(rule)
+
+    collector.WherePasses(parameter_filter)
+
+    return collector
+
+
+def titleblocks_on_sheet_collector(sheet):
+    """
+    Returns all Titleblocks on Sheet
+    """
+    collector = DB.FilteredElementCollector(
+        sheet.Document).OwnedByView(sheet.Id)
+    collector.OfCategory(DB.BuiltInCategory.OST_TitleBlocks)
+
+    return collector
+
+
+def collect_materials(doc=revit.doc):
+    collector = DB.FilteredElementCollector(doc)
+    collector.OfClass(clr.GetClrType(DB.Material))
+    collector.Cast[DB.Material]
+
+    return collector
+
+# ############
+# Get or search for some Element by name or other parameter
+# ############
+
+def get_view_by_name(name, doc=revit.doc):
+    collector = DB.FilteredElementCollector(doc)
+    collector.OfClass(DB.View)
+    collector.WhereElementIsNotElementType()
+
+    collector.WherePasses(filter.view_name_filter(name))
+
+    count = collector.GetElementCount()
+
+    if count == 1:
+        return collector.FirstElement()
+    else:
+        logger.warning("More than one view found.")
+        raise KeyError
+
+
+def get_sheet_by_number(sheet_number):
+    provider = DB.ParameterValueProvider(
+        DB.ElementId(DB.BuiltInParameter.SHEET_NUMBER))
+    rule = DB.FilterStringRule(
+        provider, DB.FilterStringEquals(), sheet_number, False)
+    parameter_filter = DB.ElementParameterFilter(rule)
+
+    collector = DB.FilteredElementCollector(revit.doc)
+    collector.OfCategory(DB.BuiltInCategory.OST_Sheets)
+
+    collector.WherePasses(parameter_filter)
+
+    # logger.debug(collector.GetElementCount())
+
+    return collector.FirstElement()
+
+
+def get_doc_by_name(doc_name, filter_func=lambda x: True):
+    for doc in revit.docs:
+        if filter_func(doc) and doc.Title == doc_name:
+            return doc
+
+
+def get_material_by_name(material_name, doc):
+    collector = material_collector(doc)
+
+    enumerable = collector.Where(lambda x: x.Name == material_name)
+
+    LOGGER.debug(enumerable.Count())
+
+    if not enumerable.Empty():
+        return enumerable.First()
+
+
+def get_element_by_name(name, hint=None, doc=revit.doc):
+    pass
