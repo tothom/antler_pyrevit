dbbf29afec80c7b8bf22f89444df590558fa8766
tothom <mail@thomasholth.no>
2022-05-02 17:31:19 +0200

    Merge: quick image

--- PyRevit Extension/Antler.extension/Antler.tab/Print.panel/print.stack/QuickImageExport.pushbutton/script.py
+++ PyRevit Extension/Antler.extension/Antler.tab/Print.panel/print.stack/QuickImageExport.pushbutton/script.py
@@ -3,7 +3,7 @@
 from System.Collections.Generic import *
 from rpw import revit, DB, UI
 
-from pyrevit import forms, EXEC_PARAMS
+from pyrevit import forms, EXEC_PARAMS, script
 from collections import OrderedDict
 
 import csv
@@ -11,6 +11,18 @@ import json
 
 uidoc = revit.uidoc
 doc = revit.doc
+
+logger = script.get_logger()
+
+
+def get_view_pixel_resolution(view, dpi):
+    bbox = view.Outline
+
+    u_dim = bbox.Max.U - bbox.Min.U
+    v_dim = bbox.Max.V - bbox.Min.V
+
+    return (int(u_dim * 12 * dpi), int(v_dim * 12 * dpi))
+
 
 # Get file name
 try:
@@ -23,42 +35,47 @@ except:
         project_name=doc.ProjectInformation.Name,
         view_name=uidoc.ActiveView.Name)
 
-# EXPORT IMAGE FROM VIEW
-file_path = forms.save_file(file_ext='png', default_name=default_name)
 
+resolutions = (
+    (72, DB.ImageResolution.DPI_72),
+    (150, DB.ImageResolution.DPI_150),
+    (300, DB.ImageResolution.DPI_300),
+    (600, DB.ImageResolution.DPI_600),
+)
 
-if file_path:
-    dpi_options = {
-        72: DB.ImageResolution.DPI_72,
-        150: DB.ImageResolution.DPI_150,
-        300: DB.ImageResolution.DPI_300,
-        600: DB.ImageResolution.DPI_600,
-    }
+# [logger.info(get_view_pixel_resolution(revit.uidoc.ActiveView, dpi)) for dpi in dpi_options.keys()]
+dpi_options = OrderedDict()
 
-    dpi_options = OrderedDict(sorted(dpi_options.items(), key=lambda t: t[0]))
+for dpi, image_resolution in resolutions:
+    pixel_resolution = get_view_pixel_resolution(revit.uidoc.ActiveView, dpi)
 
-    selected_option = forms.CommandSwitchWindow.show(
-        dpi_options.keys(),
-        message='Image DPI'
-    )
+    dpi_options["{} {}".format(dpi, pixel_resolution)] = image_resolution
 
-    image_export_options = DB.ImageExportOptions()
+selected_option = forms.CommandSwitchWindow.show(
+    dpi_options.keys(),
+    message='Image DPI'
+) or script.exit()
 
-    image_export_options.FilePath = file_path
+file_path = forms.save_file(
+    file_ext='png', default_name=default_name) or script.exit()
 
-    image_export_options.ZoomType = DB.ZoomFitType.Zoom
-    image_export_options.Zoom = 100
+image_export_options = DB.ImageExportOptions()
 
-    # image_export_options.FitDirection = DB.FitDirectionType.Horizontal
-    # image_export_options.PixelSize = 1024
+image_export_options.FilePath = file_path
 
-    image_export_options.HLRandWFViewsFileType = DB.ImageFileType.PNG
-    image_export_options.ShadowViewsFileType = DB.ImageFileType.PNG
-    image_export_options.ImageResolution = dpi_options[selected_option]
+image_export_options.ZoomType = DB.ZoomFitType.Zoom
+image_export_options.Zoom = 100
 
-    if EXEC_PARAMS.config_mode:
-        image_export_options.ExportRange = DB.ExportRange.VisibleRegionOfCurrentView
-    else:
-        image_export_options.ExportRange = DB.ExportRange.CurrentView
+# image_export_options.FitDirection = DB.FitDirectionType.Horizontal
+# image_export_options.PixelSize = 1024
 
-    doc.ExportImage(image_export_options)
+image_export_options.HLRandWFViewsFileType = DB.ImageFileType.PNG
+image_export_options.ShadowViewsFileType = DB.ImageFileType.PNG
+image_export_options.ImageResolution = dpi_options[selected_option]
+
+if EXEC_PARAMS.config_mode:
+    image_export_options.ExportRange = DB.ExportRange.VisibleRegionOfCurrentView
+else:
+    image_export_options.ExportRange = DB.ExportRange.CurrentView
+
+doc.ExportImage(image_export_options)
